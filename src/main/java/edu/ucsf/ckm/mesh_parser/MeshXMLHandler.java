e0913e51e295774b9a514ee5274e38bfe0028c4b
Jonathan Johnson <jon.johnson@ucsf.edu>
2014-07-08 10:21:49 -0700

    Write mesh files to disk instead of DB

--- src/main/java/edu/ucsf/ckm/mesh_parser/MeshXMLHandler.java
+++ src/main/java/edu/ucsf/ckm/mesh_parser/MeshXMLHandler.java
@@ -1,10 +1,10 @@
 package edu.ucsf.ckm.mesh_parser;
 
-import java.sql.Connection;
-import java.sql.Statement;
 import java.util.Date;
 import java.util.Stack;
 import java.util.logging.Logger;
+
+import java.io.*;
 
 import edu.ucsf.ckm.mesh_parser.model.Concept;
 import edu.ucsf.ckm.mesh_parser.model.Descriptor;
@@ -20,7 +20,7 @@ import org.xml.sax.helpers.DefaultHandler;
  * This assumes that we're going to be getting valid MeSH XML; there's no schmancy plan-b's should
  *  we encounter different structures than anticipated. (Not the end of the world of course - just
  *  drop the unfinished table data)
- * 
+ *
  * (Yes - package level protection)
  */
 class MeshXMLHandler
@@ -28,38 +28,31 @@ class MeshXMLHandler
         implements MeshXMLConstants {
 
     static protected final Logger LOGGER = Logger.getLogger("edu.ucsf.ckm.mesh_parser");
-    
-    
-    protected Connection dbConnection;
-
 
     // alternatively we could have kept elements as bit masks and the current location as
     //      a compilation operation on those masks
     protected Stack<String> elementStack;
     protected StringBuilder currentStringBuffer;
-    
-    
+
+
     protected Descriptor currentDescriptor;
 
     protected Concept currentConcept;
     protected SemanticType currentSemanticType;
     protected Term currentTerm;
     protected Qualifier currentQualifier;
-    
-    MeshXMLHandler (Connection c)
-            throws Exception {
-        this.dbConnection = c;
-        
-        if (c != null) {
-            this.dbConnection.setAutoCommit(false);
-        }
 
+    protected BufferedWriter outputWriter;
+
+    MeshXMLHandler (BufferedWriter outputWriter)
+            throws Exception {
+        this.outputWriter = outputWriter;
         this.elementStack = new Stack<String>();
         this.currentStringBuffer = new StringBuilder();
-        
+
         this.currentDescriptor = null;
     }
-    
+
 	/*
 	 * general game plan here:
 	 * 	look for descriptor record open and close. on close, write schleck to the db. care about
@@ -87,29 +80,27 @@ class MeshXMLHandler
 	 * 						term
 	 * 							termUI +
 	 * 							string +
-	 * 
+	 *
 	 */
-    
+
     /**
      * @Override
      */
     public void startDocument () {
         String msg = "Parse consumption starting at " + (new Date()).toString();
-        
+
         LOGGER.info(msg);
-        
+
         System.out.println(msg);
     }
-    
+
     /**
      * @Override
      */
     public void endDocument () {
         try {
             String msg = null;
-            
-            this.dbConnection.close();
-            
+
             msg = "Parse consumption finished at " + (new Date()).toString();
             LOGGER.info(msg);
             System.out.println(msg);
@@ -118,7 +109,7 @@ class MeshXMLHandler
             LOGGER.severe("Exception caught on attempting to close db connection: " + e);
         }
     }
-	
+
     /**
      * @Override
      */
@@ -126,7 +117,7 @@ class MeshXMLHandler
             throws SAXException {
         this.currentStringBuffer.append(ch, start, length);
     }
-    
+
     /**
      * @Override
      */
@@ -134,35 +125,35 @@ class MeshXMLHandler
         String elementName = qName;
 
         this.elementStack.push(elementName);
-        
+
         if (elementName.equals(DESCRIPTOR_RECORD)) {
             assert (this.currentDescriptor == null)
                                     : "Encountered descriptor inside already open descriptor";
-            
+
             this.currentDescriptor = new Descriptor();
         }
         else if (elementName.equals(QUALIFIER_REFERRED_TO) && (this.elementStack.size() == 5)) {
             assert (this.currentQualifier == null)
                                     : "Encountered qualifier inside already open qualifier";
-            
+
             this.currentQualifier = new Qualifier();
         }
         else if (elementName.equals(CONCEPT)) {
             String str = null;
-            
+
             assert (this.currentConcept == null)
                                     : "Encountered concept inside already open concept";
-            
+
             this.currentConcept = new Concept();
-            
+
             for (int i = 0; i < atts.getLength(); i++) {
                 str = atts.getLocalName(i);
-                
+
                 if (str.equals(PREFERRED_CONCEPT)) {
                     str = atts.getValue(i);
-                    
+
                     this.currentConcept.setPreferred(str.equals("Y"));
-                    
+
                     break;
                 }
             }
@@ -170,37 +161,37 @@ class MeshXMLHandler
         else if (elementName.equals(SEMANTIC_TYPE)) {
             assert (this.currentSemanticType == null)
                                     : "Encountered semantic type inside already open semantic type";
-            
+
             this.currentSemanticType = new SemanticType();
         }
         else if (elementName.equals(TERM)) {
             String str = null;
-            
+
             assert (this.currentTerm == null) : "Encountered term inside already open term";
-            
+
             this.currentTerm = new Term();
-            
+
             for (int i = 0; i < atts.getLength(); i++) {
                 str = atts.getLocalName(i);
-                
+
                 if (str.equals(CONCEPT_PREFERRED_TERM)) {
                     str = atts.getValue(i);
-                    
+
                     this.currentTerm.setConceptPreferred(str.equals("Y"));
                 }
                 else if (str.equals(PERMUTED_TERM)) {
                     str = atts.getValue(i);
-                    
+
                     this.currentTerm.setPermuted(str.equals("Y"));
                 }
                 else if (str.equals(PRINT_FLAG)) {
                     str = atts.getValue(i);
-                    
+
                     this.currentTerm.setPrint(str.equals("Y"));
                 }
                 else if (str.equals(RECORD_PREFERRED_TERM)) {
                     str = atts.getValue(i);
-                    
+
                     this.currentTerm.setRecordPreferred(str.equals("Y"));
                 }
                 else if (str.equals(LEXICAL_TAG)) {
@@ -217,17 +208,17 @@ class MeshXMLHandler
         String popped = this.elementStack.pop();
         int stackSize = this.elementStack.size();
         String elementName = qName;
-     
+
         if (! popped.equals(elementName)) {
             LOGGER.severe("Popped unmatching ending element ([" + popped + "] != [" + elementName
                           + "])");
-            
+
             return;
         }
-        
+
         if (elementName.equals(DESCRIPTOR_RECORD)) {
             this.digestAndWriteDescriptorToDB(this.currentDescriptor);
-            
+
             this.currentDescriptor = null;
         }
         else if (elementName.equals(DESCRIPTOR_UI) && (stackSize == 2)) {
@@ -235,7 +226,7 @@ class MeshXMLHandler
         }
         else if (elementName.equals(STRING)) {
             String peeked = this.elementStack.peek();
-            
+
             if (peeked.equals(CONCEPT_NAME) && (stackSize == 5)) {
                 this.currentConcept.setName(this.dumpAndCleanBuffer());
             }
@@ -260,7 +251,7 @@ class MeshXMLHandler
         }
         else if (elementName.equals(QUALIFIER_REFERRED_TO) && (stackSize == 4)) {
             this.currentDescriptor.addQualifier(this.currentQualifier);
-            
+
             this.currentQualifier = null;
         }
         else if (elementName.equals(QUALIFIER_UI) && (stackSize == 5)) {
@@ -268,7 +259,7 @@ class MeshXMLHandler
         }
         else if (elementName.equals(CONCEPT)) {
             this.currentDescriptor.addConcept(this.currentConcept);
-            
+
             this.currentConcept = null;
         }
         else if (elementName.equals(CONCEPT_UI) && (stackSize == 4)) {
@@ -288,7 +279,7 @@ class MeshXMLHandler
         }
         else if (elementName.equals(SEMANTIC_TYPE)) {
             this.currentConcept.addSemanticType(this.currentSemanticType);
-            
+
             this.currentSemanticType = null;
         }
         else if (elementName.equals(SEMANTIC_TYPE_UI)) {
@@ -299,90 +290,87 @@ class MeshXMLHandler
         }
         else if (elementName.equals(TERM)) {
             this.currentConcept.addTerm(this.currentTerm);
-            
+
             this.currentTerm = null;
         }
         else if (elementName.equals(TERM_UI) && (stackSize == 6)) {
             this.currentTerm.setUniqueId(this.dumpAndCleanBuffer());
         }
-        
+
         if (this.currentStringBuffer.length() > 0) {
             this.currentStringBuffer = null;
-            
+
             this.currentStringBuffer = new StringBuilder();
         }
     }
 
     protected String dumpAndCleanBuffer () {
         String rhett = this.currentStringBuffer.toString();
-        
+
         this.currentStringBuffer = null;
         this.currentStringBuffer = new StringBuilder();
-        
+
         return rhett.trim();
     }
-    
+
     protected void digestAndWriteDescriptorToDB (Descriptor descriptor) {
-        StringBuilder sqlStatements = new StringBuilder();
-        
         try {
-            Statement statement = this.dbConnection.createStatement();
             String sql = null;
-            
+
            // mesh_descriptor
             sql = this.startingInsertSQLForTable("mesh_descriptor") + descriptor.getUniqueId()
                     + "', '" + this.escapedString(descriptor.getName()) + "', ";
-            
+
             if (descriptor.getAnnotation() != null) {
                 sql += "'" + this.escapedString(descriptor.getAnnotation()) + "'";
             }
             else {
                 sql += "NULL";
             }
-            
+
             sql += ")";
-            
-            this.executeStatementUpdate(statement, sql, sqlStatements);
-            
+
+            this.writeSQLToFile(sql);
+
            // mesh_previous_indexing
             for (String previousIndexing : descriptor.getPreviousIndexings()) {
                 sql = this.startingInsertSQLForTable("mesh_previous_indexing")
                         + descriptor.getUniqueId() + "', '" + this.escapedString(previousIndexing)
                         + "')";
-            
-                this.executeStatementUpdate(statement, sql, sqlStatements);
+
+                this.writeSQLToFile(sql);
             }
-            
+
            // mesh_tree_x_descriptor
             for (String treeNumber : descriptor.getTreeNumbers()) {
                 sql = this.startingInsertSQLForTable("mesh_tree_x_descriptor") + treeNumber
                         + "', '" + descriptor.getUniqueId() + "')";
-            
-                this.executeStatementUpdate(statement, sql, sqlStatements);
+
+                this.writeSQLToFile(sql);
             }
-            
+
             // mesh_qualifier
             // mesh_descriptor_x_qualifier
              for (Qualifier qualifier : descriptor.getAllowableQualifiers()) {
                  sql = this.startingInsertSQLForTable("mesh_qualifier") + qualifier.getUniqueId()
                              + "', '" + this.escapedString(qualifier.getName()) + "')";
-                 
-                 this.executeStatementUpdate(statement, sql, sqlStatements);
-                 
-                 
+
+                 this.writeSQLToFile(sql);
+
+
                  sql = this.startingInsertSQLForTable("mesh_descriptor_x_qualifier")
                              + descriptor.getUniqueId() + "', '" + qualifier.getUniqueId() + "')";
-                 
-                 this.executeStatementUpdate(statement, sql, sqlStatements);
+
+                 this.writeSQLToFile(sql);
              }
-            
+
             for (Concept concept : descriptor.getConcepts()) {
                // mesh_concept
                 sql = this.startingInsertSQLForTable("mesh_concept") + concept.getUniqueId()
                         + "', '" + this.escapedString(concept.getName()) + "', '"
                         + concept.getConceptUMLSUI() + "', " + (concept.isPreferred() ? 1 : 0)
                         + ", ";
-                
+
                 if (concept.getScopeNote() != null) {
                     sql += "'" + this.escapedString(concept.getScopeNote()) + "'";
                 }
@@ -390,7 +378,7 @@ class MeshXMLHandler
                     sql += "NULL";
                 }
                 sql += ", ";
-                
+
                 if (concept.getCASN1Name() != null) {
                     sql += "'" + this.escapedString(concept.getCASN1Name()) + "'";
                 }
@@ -398,111 +386,95 @@ class MeshXMLHandler
                     sql += "NULL";
                 }
                 sql += ", ";
-                
+
                 if (concept.getRegistryNumber() != null) {
                     sql += "'" + this.escapedString(concept.getRegistryNumber()) + "'";
                 }
                 else {
                     sql += "NULL";
                 }
-                
+
                 sql += ")";
 
-                this.executeStatementUpdate(statement, sql, sqlStatements);
-                
+                this.writeSQLToFile(sql);
+
                // mesh_term
                // mesh_concept_x_term
                 for (Term term : concept.getTerms()) {
                     sql = this.startingInsertSQLForTable("mesh_term") + term.getUniqueId()
                             + "', '" + this.escapedString(term.getName()) + "', ";
-                    
+
                     if (term.getLexicalTag() != null) {
                         sql += "'" + term.getLexicalTag() + "'";
                     }
                     else {
                         sql += "NULL";
                     }
-                    
+
                     sql += this.appendingSQLForBoolean(term.isConceptPreferred());
                     sql += this.appendingSQLForBoolean(term.isRecordPreferred());
                     sql += this.appendingSQLForBoolean(term.isPermuted());
                     sql += this.appendingSQLForBoolean(term.isPrint());
-                    
+
                     sql += ")";
-                    
-                    this.executeStatementUpdate(statement, sql, sqlStatements);
-                    
-                    
+
+                    this.writeSQLToFile(sql);
+
+
                     sql = this.startingInsertSQLForTable("mesh_concept_x_term")
                                 + concept.getUniqueId() + "', '" + term.getUniqueId() + "')";
-                    
-                    this.executeStatementUpdate(statement, sql, sqlStatements);
+
+                    this.writeSQLToFile(sql);
                 }
-                
+
                // mesh_semantic_type
                // mesh_concept_x_semantic_type
                 for (SemanticType type : concept.getSemanticTypes()) {
                     sql = this.startingInsertSQLForTable("mesh_semantic_type") + type.getUniqueId()
                                 + "', '" + this.escapedString(type.getName()) + "')";
-                    
-                    this.executeStatementUpdate(statement, sql, sqlStatements);
-                    
-                    
+
+                    this.writeSQLToFile(sql);
+
+
                     sql = this.startingInsertSQLForTable("mesh_concept_x_semantic_type")
                                 + concept.getUniqueId() + "', '" + type.getUniqueId() + "')";
-                    
-                    this.executeStatementUpdate(statement, sql, sqlStatements);
+
+                    this.writeSQLToFile(sql);
                 }
-                
-                
+
+
                // mesh_descriptor_x_concept
                 sql = this.startingInsertSQLForTable("mesh_descriptor_x_concept")
                         + concept.getUniqueId() + "', '" + descriptor.getUniqueId() + "')";
-                
-                this.executeStatementUpdate(statement, sql, sqlStatements);
+
+                this.writeSQLToFile(sql);
             }
-            
-            this.dbConnection.commit();
-            
+
             LOGGER.info(". wrote tables for descriptor:" + descriptor.getUniqueId() + " - "
                          + descriptor.getName());
         }
         catch (Exception e) {
             LOGGER.severe("!! FAILED TO INSERT DESCRIPTOR: " + descriptor.getUniqueId());
-            
-            System.out.println("Failed to insert for [" + descriptor.getUniqueId() + "]\n"
-                              + sqlStatements.toString() + "\n--------");
-
-            try {
-                this.dbConnection.rollback();
-            }
-            catch (Exception ee) {}
         }
     }
-    
+
     protected String escapedString (String str) {
         return str.replaceAll("'", "\\\\'");
     }
-    
+
     protected String startingInsertSQLForTable (String tableName) {
         return "INSERT IGNORE INTO " + tableName + " VALUES ('";
     }
-    
+
     protected String appendingSQLForBoolean (boolean flag) {
         return ", " + (flag ? 1 : 0);
     }
-    
-    // this is here to have a simple point to turn on and off the db writing
-    protected void executeStatementUpdate (Statement statement, String sql, StringBuilder sb)
-            throws Exception {
-//        System.out.println(sql + ";");
-        
-        if (sb.length() > 0) {
-            sb.append('\n');
-        }
-        sb.append(sql).append(';');
-        
-        statement.executeUpdate(sql);
+
+    protected void writeSQLToFile(String sql)
+        throws Exception {
+        this.outputWriter.write(sql + ";");
+        this.outputWriter.newLine();
+        this.outputWriter.flush();
     }
-    
+
 }
